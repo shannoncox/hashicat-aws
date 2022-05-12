9f4114e2186755ae066b5933770ced4e0db7d38c
shannoncox <shannoncox@protonmail.com>
2022-05-12 20:54:53 +0000

    Added the second attempt 4 tag

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Name = "devops"
-    Name = "Department, Billable"
+    Name = ["Department], ["Billable"]
   }
 }
 
